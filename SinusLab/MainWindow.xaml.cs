bb4241bfa0b156d01bce9e6962de210cfcfb5d09
Tom Arrow <davorfilms@googlemail.com>
2021-04-05 19:18:02 +0200

    rudimentary decode!

--- SinusLab/MainWindow.xaml.cs
+++ SinusLab/MainWindow.xaml.cs
@@ -27,7 +27,8 @@ namespace SinusLab
             InitializeComponent();
 
             //quickTest();
-            imgTest2();
+            //imgTest2();
+            imgTest2_reverse();
 
             Close();
         }
@@ -81,6 +82,7 @@ namespace SinusLab
             double phaseLengthHere;
             double phaseAdvancementHere;
             double phaseHere;
+            double hueTo0to1Range;
             output[0] = 0;
             Vector3 tmpV;
             for(int i = 1; i < output.Length; i++)
@@ -91,7 +93,8 @@ namespace SinusLab
 
                 tmpV = Helpers.sRGBToCIELChab(tmpV); //cielchab = Luma, chroma, hue. hue is frequency, chroma is amplitude
 
-                frequencyHere = lowerFrequency + frequencyRange * ((double)tmpV.Z-Math.PI)/Math.PI; // Hue
+                hueTo0to1Range = ((double)tmpV.Z + Math.PI / 2) / Math.PI;
+                frequencyHere = lowerFrequency + frequencyRange * hueTo0to1Range; // Hue
                 phaseLengthHere = (double)samplerate/frequencyHere;
                 phaseAdvancementHere = 1 / phaseLengthHere;
                 phaseHere = lastPhase + phaseAdvancementHere;
@@ -110,8 +113,98 @@ namespace SinusLab
                 Array.Copy(tmp, 0, outputBytes, i * 4 *2 + 4, 4);
             }
 
-            File.WriteAllBytes("test-img3stereo.raw",outputBytes);
+            File.WriteAllBytes("test-img3stereo2.raw",outputBytes);
 
+        }
+        private void imgTest2_reverse()
+        {
+            byte[] sourceData = File.ReadAllBytes("test-img3stereo2.raw"); // 8 bit image
+            int samplerate = 48000;
+
+            int lowerFrequency = 500;
+            int upperFrequency = 20000;
+            double frequencyRange = upperFrequency - lowerFrequency;
+
+            int windowSize = 64;
+
+
+            double[] decode = new double[sourceData.Length / 8 + windowSize]; // leave windowSize amount of zeros at beginning to avoid if later.
+            double[] decodeL = new double[sourceData.Length / 8];
+
+            for(int i = 0; i < decodeL.Length; i++)
+            {
+                decodeL[i] = BitConverter.ToSingle(sourceData, i * 4 * 2);
+                decode[i + windowSize] = BitConverter.ToSingle(sourceData, i * 4 * 2+4);
+            }
+
+            double[] audioPart = new double[windowSize];
+
+            double[] freqs;
+
+            //double[] c = new double[decodeL.Length];
+            //double[] h = new double[decodeL.Length];
+
+            byte[] output = new byte[decodeL.Length * 3];
+
+
+            double[] fftMagnitude = FftSharp.Transform.FFTmagnitude(audioPart);
+            freqs = FftSharp.Transform.FFTfreq(samplerate, fftMagnitude.Length);
+
+            double tmpMaxIntensity = 0;
+            int tmpMaxIntensityIndex = 0;
+
+            double peakFrequencyHere = 0;
+            double hue = 0;
+
+            Vector3 tmpV;
+
+            // decode c,h components
+            for (int i = 0; i < decodeL.Length; i++)
+            {
+                Array.Copy(decode, i, audioPart, 0, windowSize);
+                double[] window = FftSharp.Window.Hanning(audioPart.Length);
+                FftSharp.Window.ApplyInPlace(window, audioPart);
+                fftMagnitude = FftSharp.Transform.FFTmagnitude(audioPart);
+
+                tmpMaxIntensity = 0;
+                // find biggest frequency
+                for (int b = 0; b < freqs.Length; b++)
+                {
+                    if(fftMagnitude[b] > tmpMaxIntensity)
+                    {
+                        tmpMaxIntensity = fftMagnitude[b];
+                        tmpMaxIntensityIndex = b;
+                    }
+                }
+
+
+                if(tmpMaxIntensityIndex == 0)
+                {
+                    peakFrequencyHere = (freqs[0]*fftMagnitude[0]+freqs[1] * fftMagnitude[1])/(fftMagnitude[0]+ fftMagnitude[1]);
+                } else if (tmpMaxIntensityIndex == freqs.Length - 1)
+                {
+                    peakFrequencyHere = (freqs[freqs.Length - 1] * fftMagnitude[freqs.Length - 1] + freqs[freqs.Length - 2] * fftMagnitude[freqs.Length - 2]) / (fftMagnitude[freqs.Length - 1] + fftMagnitude[freqs.Length - 2]);
+                }
+                else
+                {
+                    peakFrequencyHere = (freqs[tmpMaxIntensityIndex - 1] * fftMagnitude[tmpMaxIntensityIndex - 1] + freqs[tmpMaxIntensityIndex ] * fftMagnitude[tmpMaxIntensityIndex ] +freqs[tmpMaxIntensityIndex +1] * fftMagnitude[tmpMaxIntensityIndex + 1]) / (fftMagnitude[tmpMaxIntensityIndex - 1]+ fftMagnitude[tmpMaxIntensityIndex ]+ fftMagnitude[tmpMaxIntensityIndex + 1]);
+
+                }
+
+                hue = (((peakFrequencyHere-lowerFrequency)/frequencyRange)*Math.PI)-Math.PI/2;
+
+                tmpV.X = (float)(decodeL[i]/2+0.5)*100;
+                tmpV.Y = (float)Math.Sqrt(tmpMaxIntensity)*100; //experimental * 4, normally doesnt beong there.
+                tmpV.Z = (float)hue;
+
+                tmpV = Helpers.CIELChabTosRGB(tmpV);
+
+                output[i * 3] = (byte)Math.Min(255,Math.Max(0,tmpV.X));
+                output[i * 3+1] = (byte)Math.Min(255,Math.Max(0,tmpV.Y));
+                output[i * 3+2] = (byte)Math.Min(255,Math.Max(0,tmpV.Z));
+            }
+
+            File.WriteAllBytes("test-img3stereo2-backtoIMG4.raw",output);
         }
 
         private void imgTest()
