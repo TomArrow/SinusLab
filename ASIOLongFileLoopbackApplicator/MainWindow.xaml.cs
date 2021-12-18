8195cf1d7ea2f6bf7161973f8cebef05fee74bda
Tom Arrow <davorfilms@googlemail.com>
2021-12-18 16:07:10 +0100

    Created a new tool to allow to record a loopback through ASIO with no length limitations. Should be neat.

---
+++ ASIOLongFileLoopbackApplicator/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASIOLongFileLoopbackApplicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string selectedAsioDevice = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            asioDevice.ItemsSource = AsioOut.GetDriverNames();
+            
+        }
+
+
+
+        private void asioDevice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedAsioDevice = (string)asioDevice.SelectedItem;
+            goBtn.IsEnabled = true;
+        }
+
+        private void goBtn_Click(object sender, RoutedEventArgs e)
+        {
+            processAudioFile();
+        }
+
+        const int EXTRARECORDINGTIME = 10;// Extra recording time in seconds. Make sure we dont miss anything.
+
+        private void processAudioFile()
+        {
+            goBtn.IsEnabled = false;
+            asioDevice.IsEnabled = false;
+
+            bool fail = true;
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select w64 file to process";
+
+            if (ofd.ShowDialog() == true)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+
+                string suffix = "";
+                
+
+                sfd.FileName = ofd.FileName + ".ASIOLFLA" + suffix + ".w64";
+                if (sfd.ShowDialog() == true)
+                {
+                    SuperWAV inputAudio = new SuperWAV(ofd.FileName);
+                    SuperWAV.WavInfo inInfo = inputAudio.getWavInfo();
+
+                    if(inInfo.channelCount != 2)
+                    {
+                        MessageBox.Show("Only files with a channel count of 2 supported.");
+                    } else
+                    {
+
+                        fail = false;
+
+                        // Allocate an output file.
+                        SuperWAV outputAudio = new SuperWAV(sfd.FileName, SuperWAV.WavFormat.WAVE64, inInfo.sampleRate, 2,inInfo.audioFormat,inInfo.bitsPerSample);
+
+                        ConcurrentQueue<float[]> sampleBuffer = new ConcurrentQueue<float[]>();
+                        float[] tmp;
+
+                        bool processingActive = true;
+
+                        UInt64 outputFileOffset = 0;
+
+                        // Do actual processing.
+                        AsioOut asioOut = new AsioOut(selectedAsioDevice);
+                        SuperWAVProvider sampleProvider = new SuperWAVProvider(inputAudio);
+                        IWaveProvider waveProvider = new SampleToWaveProvider(sampleProvider);
+                        asioOut.InitRecordAndPlayback(waveProvider, 2, (int)inInfo.sampleRate);
+                        asioOut.AudioAvailable += (object sender, AsioAudioAvailableEventArgs e) =>{
+                            sampleBuffer.Enqueue(e.GetAsInterleavedSamples());
+                        };
+
+                        _ = Task.Run(()=>{ 
+
+                            Task writerTask = Task.Run(()=> {
+                                float[] tmp2;
+                                while (processingActive)
+                                {
+
+                                    while(sampleBuffer.Count > 0)
+                                    {
+                                        if (!processingActive)
+                                        {
+                                            break;
+                                        }
+                                        if(sampleBuffer.TryDequeue(out tmp2))
+                                        {
+                                            outputAudio.writeFloatArrayFast(tmp2, outputFileOffset);
+                                            outputFileOffset += (ulong)tmp2.Length / 2;
+                                        }
+                                    }
+
+                                    System.Threading.Thread.Sleep(10); // Just make sure we don't eat up all CPU with constant checks of the queue, pointless
+                                }
+                            });
+                            // Progress updates
+                            Task progressTask = Task.Run(()=> {
+                                while (processingActive)
+                                                                                {
+                                    double progress = (double)sampleProvider.InputOffset / (inputAudio.DataLengthInTicks + inInfo.sampleRate * EXTRARECORDINGTIME);
+                                    Dispatcher.Invoke(() => {
+                                        progressBar.Value = progress * 100.0;
+                                    });
+                                    if (progress > 1.0)
+                                    {
+                                        processingActive = false;
+                                        asioOut.Stop();
+                                    }
+                                    System.Threading.Thread.Sleep(100);
+                                }
+                            });
+                            writerTask.Wait();
+                            progressTask.Wait();
+                            Dispatcher.Invoke(() => {
+                                goBtn.IsEnabled = true;
+                                asioDevice.IsEnabled = true;
+                            });
+
+                        });
+
+
+                        asioOut.Play(); // start recording
+                    }
+
+                }
+            }
+            
+                    
+            
+
+            if (fail)
+            {
+                goBtn.IsEnabled = true;
+                asioDevice.IsEnabled = true;
+            }
+
+        }
+    }
+}
---
+++ ASIOLongFileLoopbackApplicator/SuperWAVProvider.cs
@@ -0,0 +1,63 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASIOLongFileLoopbackApplicator
+{
+    class SuperWAVProvider : ISampleProvider
+    {
+        private SuperWAV inputFile;
+        private SuperWAV.WavInfo wavInfo;
+
+        private bool endReached = false;
+
+        private UInt64 inputOffset = 0;
+
+        WaveFormat waveFormat = null;
+        public WaveFormat WaveFormat { get {
+                return waveFormat;
+            }  }
+
+        public bool EndReached { get
+            {
+                return endReached;
+            } }
+        public UInt64 InputOffset
+        { get
+            {
+                return inputOffset;
+            } }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            UInt64 offsetAdd = ((ulong)count / wavInfo.channelCount);
+            float[] readTicks = inputFile.getAs32BitFloatFast(inputOffset, inputOffset+ offsetAdd +1); // The +1 is because it could round down. Just to be safe
+            inputOffset += offsetAdd;
+            if(readTicks.Length == 0)
+            {
+                // End reached
+                endReached = true;
+            }
+            UInt64 countToCopy = Math.Min((ulong)count, (UInt64) readTicks.Length); // Make sure we don't accidentally copy too much
+            //Array.Copy(readTicks, 0, buffer, offset,(int) countToCopy); // this doesnt wanna work, bah
+
+            for (ulong n = 0; n < countToCopy; n++)
+            {
+                buffer[offset + (int)n] = readTicks[n];
+            }
+
+            return (int)countToCopy;
+        }
+
+        public SuperWAVProvider(SuperWAV inputFileA)
+        {
+            inputFile = inputFileA;
+            wavInfo = inputFile.getWavInfo();
+            waveFormat = WaveFormat.CreateIeeeFloatWaveFormat((int)wavInfo.sampleRate, wavInfo.channelCount); 
+            // Float is how we deliver.
+        }
+    }
+}
